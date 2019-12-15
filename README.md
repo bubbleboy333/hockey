9a02b1c93d7e76cf2eab9a9a794bd9a8b325aff3
Andrew Rexroth <arrexrot@amazon.com>
2019-12-15 15:00:19 -0800

    added links to streams on team header, updated link keys

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # hockey
 
-need to install `yattag` before running
-this is done through `pip install yattag`
+need to install `yattag` before running. This is done through `pip install yattag`  
+to be compatible with `'X-Frame-Options':'SAMEORIGIN'` header attribute the [ignore-x-frame-headers chrome extension is needed](https://chrome.google.com/webstore/detail/ignore-x-frame-headers/gleekbfjekiniecknbkamfmkohkpodhe)
 
 run format is:
 `python3 hockey.py [team names]`
--- hockey.py
+++ hockey.py
@@ -8,54 +8,91 @@ from yattag import Doc
 ### Eastern Conference
 ## Atlantic Division
 # Boston Bruins -> Boston, Massachusetts -> Boston
+boston_link_value = "Boston"
 # Buffalo Sabres -> Buffalo, New York -> Buffalo
+buffalo_link_value = "Buffalo"
 # Detroit Red Wings -> Detroit, Michigan -> Detroit
+detroit_link_value = "Detroit"
 # Florida Panthers -> Sunrise, Florida -> Florida
+florida_link_value = "Florida"
 # Montreal Canadiens -> Montreal, Quebec -> Montreal
+montreal_link_value = "Montreal"
 # Ottawa Senators -> Ottawa, Ontario -> Ottawa
+ottawa_link_value = "Ottawa"
 # Tampa Bay Lightning -> Tampa, Florida ->
 # Toronto Maple Leafs -> Toronto, Ontario -> Toronto
+toronto_link_value = "Toronto"
 ##  Metropolitan Division
 # Carolina Hurricanes -> Raleigh, North Carolina ->
 # Columbus Blue Jackets -> Columbus, Ohio ->
 # New Jersey Devils -> Newark, New Jersey ->
 # New York Islanders -> New York City, New York ->
-# Philadelphia Flyers -> Philadelphia, Pennsylvania ->
+# Philadelphia Flyers -> Philadelphia, Pennsylvania -> Philadelphia
+philadelphia_link_value = "Philadelphia"
 # Pittsburgh Penguins -> Pittsburgh, Pennsylvania ->
 # Washington Capitals -> Washington, D.C. ->
 ### Western Conference
-## Central
-# Chicago Blackhawks -> Chicago, Illinois ->
+## Central Division
+# Chicago Blackhawks -> Chicago, Illinois -> Chicago
+chicago_link_value = "Chicago"
 # Colorado Avalanche -> Denver, Colorado ->
 # Dallas Stars -> Dallas, Texas ->
-# Minnesota Wild -> Saint Paul, Minnesota ->
+# Minnesota Wild -> Saint Paul, Minnesota -> Minnesota
+minnesota_link_value = "Minnesota"
 # Nashville Predators -> Nashville, Tennessee ->
 # St. Louis Blues -> St. Louis, Missouri ->
-# Winnipeg Jets -> Winnipeg, Manitoba ->
-## Pacific
+# Winnipeg Jets -> Winnipeg, Manitoba -> Winnipeg
+winnipeg_link_value = "Winnipeg"
+## Pacific Division
 # Anaheim Ducks -> Anaheim, California ->
 # Arizona Coyotes -> Glendale, Arizona ->
 # Calgary Flames -> Calgary, Alberta ->
 # Edmonton Oilers -> Edmonton, Alberta ->
-# Los Angeles Kings -> Los Angeles, California ->
+# Los Angeles Kings -> Los Angeles, California -> Los-Angeles
+los_angeles_link_value = "Los-Angeles"
 # San Jose Sharks -> San Jose, California ->
-# Vancouver Canucks -> Vancouver, British Columbia ->
-# Vegas Golden Knights -> Paradise, Nevada ->
+# Vancouver Canucks -> Vancouver, British Columbia -> Vancouver
+vancouver_link_value = "Vancouver"
+# Vegas Golden Knights -> Paradise, Nevada -> Vegas
+vegas_link_value = "Vegas"
 # Seattle -> Seattle, Washington ->
 
 team_names = {
-    "": "",
-    "boston": "Boston",
-    "bruins": "Boston",
-    "buffalo": "Buffalo",
-    "sabres": "Buffalo",
-    "red wings": "Detroit",
-    "redwings": "Detroit",
-    "detroit": "Detroit",
-    "florida": "Florida",
-    "panthers": "Florida",
-    "montreal": "Montreal",
-    "canadiens": "Montreal",
+    # Atlantic Division
+    "boston": boston_link_value, "bruins": boston_link_value,
+    "buffalo": buffalo_link_value, "sabres": buffalo_link_value,
+    "detroit": detroit_link_value, "wings": detroit_link_value, "redwings": detroit_link_value,
+    "florida": florida_link_value, "panthers": florida_link_value,
+    "montreal": montreal_link_value, "canadiens": montreal_link_value,
+    "ottawa": ottawa_link_value, "senators": ottawa_link_value,
+    "tampabay": "", "tampa": "", "lightning": "",
+    "toronto": toronto_link_value, "mapleleafs": toronto_link_value, "leafs": toronto_link_value,
+    # Metropolitan Division
+    "carolina": "", "hurricanes": "",
+    "columbus": "", "bluejackets": "", "jackets": "",
+    "newjersey": "", "jersey": "", "devils": "",
+    "newyork": "", "islanders": "",
+    "philadelphia": philadelphia_link_value, "philly": philadelphia_link_value, "flyers": philadelphia_link_value,
+    "pittsburgh": "", "penguins": "",
+    "washington": "", "capitals": "",
+    # Central Division
+    "chicago": chicago_link_value, "blackhawks": chicago_link_value,
+    "colorado": "", "avalanche": "",
+    "dallas": "", "stars": "",
+    "minnesota": minnesota_link_value, "wild": minnesota_link_value,
+    "nashville": "", "predators": "",
+    "stlouis": "", "louis": "", "blues": "",
+    "winnipeg": winnipeg_link_value, "jets": winnipeg_link_value,
+    # Pacific Division
+    "anaheim": "", "ducks": "",
+    "arizona": "", "coyotes": "",
+    "calgary": "", "flames": "",
+    "edmonton": "", "oilers": "",
+    "losangeles": los_angeles_link_value, "angeles": los_angeles_link_value, "kings": los_angeles_link_value,
+    "sanjose": "", "jose": "", "sharks": "",
+    "vancouver": vancouver_link_value, "canucks": vancouver_link_value,
+    "vegas": vegas_link_value, "lasvegas": vegas_link_value, "goldenknights": vegas_link_value, "knights": vegas_link_value,
+    "seattle": "", "": "",
 }
 # number of arguments on CLI before team names
 num_pre_team_args = 1
@@ -78,19 +115,23 @@ def format_link(team):
 
 def build_one_team(teams):
     for team in teams:
+        team_link = format_link(team)
         with tag('h4', style="margin:0px;height:4%"):
-            text(team)
-        with tag('iframe', src=format_link(team), style="height:95%;width:100%"):
+            with tag('a', href=team_link):
+                text(team)
+        with tag('iframe', src=team_link, style="height:95%;width:100%"):
             text("")
 
 def build_two_team(teams):
     with tag('div', style="display:table;height:100%;width:100%"):
         with tag('div', style="display:table-row"):
             for team in teams:
+                team_link = format_link(team)
                 with tag('div', style="display:table-cell"):
                     with tag('h4', style="margin:0px;height:4%"):
-                        text(team)
-                    with tag('iframe', src=format_link(team), style="height:90%;width:100%"):
+                        with tag('a', href=team_link):
+                            text(team)
+                    with tag('iframe', src=team_link, style="height:95%;width:100%"):
                         text("")
 
 def build_four_team(teams):
@@ -100,9 +141,11 @@ def build_four_team(teams):
             with tag('div', style="display:table-row"):
                 for col in range(0,2):
                     with tag('div', style="display:table-cell"):
+                        team_link = format_link(teams[team_index])
                         with tag('h4', style="margin:0px;height:4%"):
-                            text(teams[team_index])
-                        with tag('iframe', src=format_link(teams[team_index]), style="height:90%;width:100%"):
+                            with tag('a', href=team_link):
+                                text(teams[team_index])
+                        with tag('iframe', src=team_link, style="height:95%;width:100%"):
                             text("")
                     team_index += 1
 
